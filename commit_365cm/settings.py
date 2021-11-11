95597ef1aeec5e0c4958da463ca557a4f1f21875
Seulg2027 <seulgi2027@dgu.ac.kr>
2021-11-11 17:51:37 +0900

    template setting

--- commit_365cm/settings.py
+++ commit_365cm/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'commit_365cm', 'static')
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- commit_365cm/urls.py
+++ commit_365cm/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from commit_365cm import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.main),
 ]
