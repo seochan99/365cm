aaa80d1d4dd7073491c6f5b1e03f385533a532ed
rawfishthelgh <rg970604@naver.com>
2021-11-15 18:33:27 +0900

    뉴스 추가

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,6 @@ app_name = "main"
 urlpatterns = [
     path('', showmain, name="showmain"),
     path('faq/', faq, name="faq"),
+    path('news/', news, name="news"),
+    path('news_detail/<str:id>', news_detail, name="news_detail"),
 ]
