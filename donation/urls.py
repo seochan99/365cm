b69643ce2b28c7c7a0e23aaddafd9ad7a0b50b7a
kkong1007 <vkslvkd1007@naver.com>
2021-11-16 17:08:56 +0900

    기부페이지 기능 구현

---
+++ donation/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from donation import views
+from .views import *
+
+
+app_name = "donation"
+urlpatterns = [
+    path('donate/', donate, name="donate"),
+    path('donate_detail/<int:id>', donate_detail, name="donate_detail"),
+    path('donate_new/', donate_new, name="donate_new"),
+    path('donate_create/', donate_create, name="donate_create"),
+    path('donate_edit/<int:id>', donate_edit, name="donate_edit"),
+    path('donate_update/<int:id>', donate_update, name="donate_update"),
+    path('donate_delete/<int:id>', donate_delete, name="donate_delete"),
+]
